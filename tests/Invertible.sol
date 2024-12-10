18a66e41056d375c7749bc9de6219fadd8b00e33
Ondrej Kurák <ondrej@certora.com>
2024-12-10 18:18:40 +0100

    Initial Setup (#1)
    * Install & Sanitize
    
    * Install solc
    
    * Use existing solc versions
    
    * Fix paths
    
    * Add comments
    
    * Add permission
    
    * Move workflow
    
    * Fix typo
    
    * Fix typo
    
    * Return version

---
+++ tests/Invertible.sol
@@ -0,0 +1,16 @@
+contract InvertibleBroken {
+	address public admin;
+	uint public counter;
+		
+	function inc() public returns (uint) {
+		require(msg.sender == admin);
+		return ++counter;
+	}
+	
+	function dec() public returns (uint) {
+		require(msg.sender == admin);
+		return --counter;
+	}
+}
+
+
