d9c7a9c5ef6115f65a374e6df40d6769e9dde9ca
Oz <oz@certora.com>
2025-07-01 11:29:08 +0300

    use CERTORA_SUBDOMAIN

--- scripts/gh-app-integration.sh
+++ scripts/gh-app-integration.sh
@@ -11,7 +11,7 @@ echo "Checking GitHub App integration..."
 # Validate required variables
 : "${CERTORA_LOG_DIR:?Missing CERTORA_LOG_DIR}"
 : "${GROUP_ID:?Missing GROUP_ID}"
-: "${CERTORA_SERVER:?Missing CERTORA_SERVER}"
+: "${CERTORA_SUBDOMAIN:?Missing CERTORA_SUBDOMAIN}"
 : "${ACTIONS_ID_TOKEN_REQUEST_TOKEN:?Missing ACTIONS_ID_TOKEN_REQUEST_TOKEN}"
 : "${ACTIONS_ID_TOKEN_REQUEST_URL:?Missing ACTIONS_ID_TOKEN_REQUEST_URL}"
 : "${GITHUB_EVENT_PATH:?Missing GITHUB_EVENT_PATH}"
@@ -28,23 +28,6 @@ fi
 PR_NUMBER="$(jq --raw-output '.pull_request.number' "$GITHUB_EVENT_PATH")"
 COMMIT="$GITHUB_SHA"
 
-# Select API domain
-case "$CERTORA_SERVER" in
-  production)
-    DATA_API="data-api"
-    ;;
-  staging)
-    DATA_API="data-api-stg"
-    ;;
-  vaas-dev)
-    DATA_API="data-api-dev"
-    ;;
-  *)
-    echo "::error title=Invalid Certora Server::Certora Server must be set to 'production', 'staging', or 'vaas-dev'."
-    exit 1
-    ;;
-esac
-
 # JSON payload
 PAYLOAD=$(jq -n \
   --arg group_id "$GROUP_ID" \
@@ -54,7 +37,7 @@ PAYLOAD=$(jq -n \
   '{group_id: $group_id, repo: $repo, commit: $commit, pr_number: $pr_number}')
 
 # Make API request to verify GitHub App integration
-curl -sSL --proto '=https' --tlsv1.2 --retry 10 --retry-connrefused --fail-with-body -X POST "https://$DATA_API.certora.com/v1/github-app/verify" \
+curl -sSL --proto '=https' --tlsv1.2 --retry 10 --retry-connrefused --fail-with-body -X POST "https://$CERTORA_SUBDOMAIN.certora.com/v1/github-app/verify" \
   -H "Authorization: Bearer $TOKEN" \
   -H "Content-Type: application/json" \
   -d "$PAYLOAD" > "$GHINT_LOG" || {
