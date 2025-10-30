b2844e15b41d98290ef09896a210df1a929e1ff8
Taz <46790424+ozcodes@users.noreply.github.com>
2025-10-30 09:02:46 +0200

    CERT-9682 | Empty comments (#62)
    * add comment if complitaion fails
    
    * failure
    
    * don't comment always
    
    * solidity repo moved to argotorg
    
    * check total_jobs and always add summary
    
    * greater than 0
    
    * line

--- scripts/solc-download.sh
+++ scripts/solc-download.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 if [ "$DEBUG_LEVEL" -gt 0 ]; then
   set -x
 fi
@@ -8,7 +10,7 @@ REMOVE_PREFIX="$1"
 
 mkdir -p /opt/solc-bin
 
-GH_LINK='https://api.github.com/repos/ethereum/solidity/releases/tags/v'
+GH_LINK='https://api.github.com/repos/argotorg/solidity/releases/tags/v'
 JQ_FILTER='.assets[] | select(.name == "solc-static-linux") | .url'
 AUTH_HEADER="Authorization: Bearer ${GITHUB_TOKEN}"
 
